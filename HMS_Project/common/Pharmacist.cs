24c3291a70150ace9cfc05272e76aa7b2e331096
AhmedAli <ahmed.khalil@std.mans.edu.eg>
2024-08-29 22:46:50 +0300

    update

---
+++ HMS_Project/common/Pharmacist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common
+{
+    public class Pharmacist
+    {
+        private int pharmacistID;
+        private string fname;
+        private string lname;
+
+        public int PharmacistID
+        {
+            get { return pharmacistID; }
+            set { pharmacistID = value; }
+        }
+
+        public string Fname
+        {
+            get { return fname; }
+            set { fname = value; }
+        }
+
+        public string Lname
+        {
+            get { return lname; }
+            set { lname = value; }
+        }
+    }
+}
+
---
+++ HMS_Project/common/Prescription.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common
+{
+    public class Prescription
+    {
+        private int prescriptionID;
+        private int dosage;
+        private DateTime dateIssued;
+        private int duration;
+      
+
+        public int PrescriptionID
+        {
+            get { return prescriptionID; }
+            set { prescriptionID = value; }
+        }
+
+        public int Dosage
+        {
+            get { return dosage; }
+            set { dosage = value; }
+        }
+
+        public DateTime DateIssued
+        {
+            get { return dateIssued; }
+            set { dateIssued = value; }
+        }
+
+        public int Duration
+        {
+            get { return duration; }
+            set { duration = value; }
+        }
+
+       
+    }
+}
